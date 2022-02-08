740a92f40ea331703fecb03f2de81958b76883f0
louis.huang <0563louishuang@gmail.com>
2022-02-08 14:28:45 -0800

    Allow contract to receive eth, and add util functions

--- contracts/BBRoyalty.sol
+++ contracts/BBRoyalty.sol
@@ -6,20 +6,33 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BBRoyalty is ERC721, Ownable {
-    string public contractURIjson; 
+    string public _contractURI; 
 
+    event Received(address indexed sender, uint256 amount);
     event Withdraw(address indexed recipient, uint256 amount);
 
-    constructor(string memory _contractURI) ERC721("BBRoyalty", "BBR") {
-       contractURIjson = _contractURI;
+    constructor(string memory _newContractURI) ERC721("BBRoyalty", "BBR") {
+       _contractURI = _newContractURI;
+    }
+
+    receive() external payable {
+      emit Received(msg.sender, msg.value);
+    }
+
+    function getBalance() public view returns(uint256) {
+      return address(this).balance;
+    }
+
+    function getAddress() external view returns(address) {
+      return address(this);
     }
     
     function contractURI() public view returns (string memory) {
-        return contractURIjson;
+        return _contractURI;
     }
 
-    function setContractURI(string memory _contractURI) external onlyOwner {
-      contractURIjson = _contractURI;
+    function setContractURI(string memory _newContractURI) external onlyOwner {
+      _contractURI = _newContractURI;
     }
 
     function withdrawToAddress(address payable _recipient, uint256 _amount) external onlyOwner {
